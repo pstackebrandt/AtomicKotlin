97ab819b016afbeaf1ab428004f37ac1cfccdefb
Peter Stackebrandt <p.stackebrandt@immowelt.de>
2019-07-11 12:32:56 +0200

    Add solution of for loop in tasks contain a long version of a task I will refactor

--- Programming Basics/The in Keyword/Exercise 2/src/Task.kt
+++ Programming Basics/The in Keyword/Exercise 2/src/Task.kt
@@ -1,7 +1,14 @@
 package theinKeyword2
 
 fun sumOfEven(n: Int): Int {
-    TODO()
+
+    var sum = 0
+    for (i in 0..n) {
+        if (i % 2 != 0) continue
+        sum += i
+    }
+
+    return sum
 }
 
 fun main(args: Array<String>) {
--- Programming Basics/The in Keyword/Exercise 4/src/Task.kt
+++ Programming Basics/The in Keyword/Exercise 4/src/Task.kt
@@ -1,12 +1,41 @@
 package theinKeyword4
 
-
-
 fun isValidIdentifier(s: String): Boolean {
-    TODO()
+    if (s.isEmpty()) return false
+
+    for ((index, char) in s.withIndex()) {
+        if (index == 0 && !getStartChars().contains(char)) return false
+        if (!getSecondaryChars().contains(char)) return false
+    }
+
+    return true
+}
+
+private fun getStartChars(): String {
+
+    val builder = StringBuilder()
+
+    ('a'..'z').forEach { letter -> builder.append(letter) }
+    ('A'..'Z').forEach { letter -> builder.append(letter) }
+    builder.append("_")
+
+    return builder.toString()
+}
+
+private fun getSecondaryChars(): String {
+    val builder = StringBuilder()
+
+    ('a'..'z').forEach { letter -> builder.append(letter) }
+    ('A'..'Z').forEach { letter -> builder.append(letter) }
+    (0..9).forEach { letter -> builder.append(letter) }
+    builder.append("_")
+
+    return builder.toString()
 }
 
 fun main(args: Array<String>) {
+    println("StartChars: ${getStartChars()}")
+    println("Secondary chars: ${getSecondaryChars()}")
     println(isValidIdentifier("name"))  // true
     println(isValidIdentifier("0name"))  // false
 }
