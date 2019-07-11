7fedd03aa64a3e6c03187740b3a8cc083fbdce8e
Peter Stackebrandt <p.stackebrandt@immowelt.de>
2019-07-11 15:18:01 +0200

    Add solution of refactored for in task

--- Programming Basics/The in Keyword/Exercise 4/src/Task.kt
+++ Programming Basics/The in Keyword/Exercise 4/src/Task.kt
@@ -1,41 +1,29 @@
 package theinKeyword4
 
+
 fun isValidIdentifier(s: String): Boolean {
+
     if (s.isEmpty()) return false
 
     for ((index, char) in s.withIndex()) {
-        if (index == 0 && !getStartChars().contains(char)) return false
-        if (!getSecondaryChars().contains(char)) return false
+        if (index == 0 && isStartChar(char)) continue
+        if (index > 0 && isCommonChar(char)) continue
+        return false
     }
-
     return true
 }
 
-private fun getStartChars(): String {
-
-    val builder = StringBuilder()
-
-    ('a'..'z').forEach { letter -> builder.append(letter) }
-    ('A'..'Z').forEach { letter -> builder.append(letter) }
-    builder.append("_")
-
-    return builder.toString()
+fun isStartChar(char: Char): Boolean {
+    if (char.isLetter() || char == '_') return true
+    return false
 }
 
-private fun getSecondaryChars(): String {
-    val builder = StringBuilder()
-
-    ('a'..'z').forEach { letter -> builder.append(letter) }
-    ('A'..'Z').forEach { letter -> builder.append(letter) }
-    (0..9).forEach { letter -> builder.append(letter) }
-    builder.append("_")
-
-    return builder.toString()
+fun isCommonChar(char: Char): Boolean {
+    if (char.isLetterOrDigit() || char == '_') return true
+    return false
 }
 
 fun main(args: Array<String>) {
-    println("StartChars: ${getStartChars()}")
-    println("Secondary chars: ${getSecondaryChars()}")
     println(isValidIdentifier("name"))  // true
     println(isValidIdentifier("0name"))  // false
 }
