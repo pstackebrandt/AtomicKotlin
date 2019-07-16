ba4a2f1ca70467ff2d862e6c267df52132d36f92
Peter Stackebrandt <p.stackebrandt@immowelt.de>
2019-07-16 17:21:11 +0200

    Add own solution for count digit

--- Programming Basics/Summary 1/Exercise 1/src/Task.kt
+++ Programming Basics/Summary 1/Exercise 1/src/Task.kt
@@ -2,9 +2,16 @@ package summary1
 
 fun countDigits(number: Int, digit: Int): Int {
     var worker = number.toString()
+    val digitString = digit.toString()
     var occurrences = 0
-    while (worker.length > 0) {
-        if (worker[worker.length-1]) == digit.toChar()
+    var lastValue: String
+
+    while (worker.isNotEmpty()) {
+        lastValue= worker[worker.length - 1].toString()
+        if (lastValue == digitString) {
+            occurrences += 1
+        }
+        worker = worker.substring(0, worker.length-1)
     }
     return occurrences
 }
