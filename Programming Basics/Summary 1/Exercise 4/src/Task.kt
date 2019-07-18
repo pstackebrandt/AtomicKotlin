b70a9d67e06025597de752f8d32a68ef53fc0c28
Peter Stackebrandt <p.stackebrandt@immowelt.de>
2019-07-18 15:49:30 +0200

    Add for solution snake summary4. Own and answer code don't fit to tests.

--- Programming Basics/Summary 1/Exercise 4/src/Task.kt
+++ Programming Basics/Summary 1/Exercise 4/src/Task.kt
@@ -1,7 +1,24 @@
 package summary4
 
 fun printSnake(rows: Int, columns: Int) {
-    TODO()
+    if (rows < 1 || columns < 1) return
+
+    val width = (rows * columns).toString().length + 1
+    var number: Int
+
+    for (row in 0 until rows) {
+        for (column in 0 until columns) {
+            number = row * columns
+            number += if (row % 2 == 0) {
+                column
+            } else {
+                columns - 1 - column
+            }
+
+            print("%${width}d".format(number))
+        }
+        println()
+    }
 }
 
 fun main(args: Array<String>) {
