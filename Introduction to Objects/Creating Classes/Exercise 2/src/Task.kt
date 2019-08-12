7eb8358d275e2b42afe215fd8ebf8881dae6c0b8
Peter Stackebrandt <peter.stackebrandt@immowelt.de>
2019-08-12 22:53:12 +0200

    Add creatingClasses2

--- Introduction to Objects/Creating Classes/Exercise 2/src/Task.kt
+++ Introduction to Objects/Creating Classes/Exercise 2/src/Task.kt
@@ -1,7 +1,14 @@
 package creatingClasses2
 
 fun isPalIgnoreCase(s: SimpleString): Boolean {
-    TODO()
+    val cleaned = StringBuilder()
+
+    for (i in 0 until s.length()){
+        cleaned.append(s.get(i)
+            .toLowerCase())
+    }
+
+    return cleaned.toString() == cleaned.reverse().toString()
 }
 
 fun main(args: Array<String>) {
