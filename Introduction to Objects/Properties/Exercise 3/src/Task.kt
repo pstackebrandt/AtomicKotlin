5b7c9e21deb5156d56f086cd091625c97f433e8a
Peter Stackebrandt <peter.stackebrandt@immowelt.de>
2019-08-14 17:38:29 +0200

    Until constructors1

--- Introduction to Objects/Properties/Exercise 3/src/Task.kt
+++ Introduction to Objects/Properties/Exercise 3/src/Task.kt
@@ -3,11 +3,13 @@ package properties3
 class Counter {
     var value = 0
 
+    private val i = 10
+
     fun inc() {
-        value += 10
+        value += i
     }
 
     fun dec() {
-        value -= 10
+        value -= i
     }
 }
