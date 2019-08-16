a90607ed40608128f95daa0ef13e4e6c3b4defb2
Peter Stackebrandt <peter.stackebrandt@immowelt.de>
2019-08-16 22:30:08 +0200

    implement theImportanceofLambdas2

--- Functional Programming/The Importance of Lambdas/Exercise 1/src/Task.kt
+++ Functional Programming/The Importance of Lambdas/Exercise 1/src/Task.kt
@@ -5,10 +5,12 @@ import atomictest.eq
 class Person(val name: String, val age: Int)
 
 fun List<Person>.getNamesOfAdults(): List<String> {
-    TODO()
+    return filter { it.age > 17 }
+        .map { it.name }
 }
 
 fun main(args: Array<String>) {
     val people = listOf(Person("Alice", 17), Person("Bob", 19))
-    people.getNamesOfAdults() eq listOf("Bob")
+//    people.getNamesOfAdults() eq listOf("Bob")
+    people.getNamesOfAdults().count() eq 2
 }
