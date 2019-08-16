b331ce53e67f42f8e3ca7c7657d34163f3d48268
Peter Stackebrandt <peter.stackebrandt@immowelt.de>
2019-08-17 00:37:28 +0200

    operationsonCollections1

--- Functional Programming/Operations on Collections/Exercise 1/src/Task.kt
+++ Functional Programming/Operations on Collections/Exercise 1/src/Task.kt
@@ -5,14 +5,22 @@ import atomictest.eq
 data class Person(val name: String, val age: Int)
 
 fun findOldest(people: List<Person>, requiredNumber: Int): List<String> {
-    TODO()
+
+    return people
+        .sortedByDescending { person -> person.age }
+        .subList(0, minOf(requiredNumber, people.count()))
+        .map { it.name }
+        .sorted()
 }
 
 fun main(args: Array<String>) {
     val people = listOf(
-            Person("Bob", 30),
-            Person("Charlie", 20),
-            Person("Alice", 25))
+        Person("Bob", 30),
+        Person("Charlie", 20),
+        Person("Alice", 25))
     findOldest(people, 1) eq listOf("Bob")
     findOldest(people, 2) eq listOf("Alice", "Bob")
+    findOldest(people, 1).count() eq 1
+    findOldest(people, 2).count() eq 2
+    findOldest(people, 3).count() eq 3
 }
